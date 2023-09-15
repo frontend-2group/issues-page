76ce1012ae5955a7cd64b4fb748058f25f9af167
LeeDaeGyeong <dleorud1027@gmail.com>
2023-09-15 23:11:33 +0900

    rtk 셋팅 및 오픈 API 데이터 받아왔습니다

--- src/components/oneIssuesList.js
+++ src/components/oneIssuesList.js
@@ -1,18 +1,16 @@
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchIssuesAsync } from "../reducer/issues";
+import { asyncIssues } from "../reducer/issues";
+import { useEffect } from "react";
 
 const OneIssuesList = () => {
-  // 컴포넌트에서 issues 배열 사용하려면 이거 써야 합니다------
   const dispatch = useDispatch();
   const { issues } = useSelector((state) => state.issues);
-  useEffect(() => {
-    if (issues === null) {
-      dispatch(fetchIssuesAsync());
-    }
-  }, [dispatch, issues]);
-  // ---------------------------------------------------------
 
+  useEffect(() => {
+    dispatch(asyncIssues());
+  }, [dispatch]);
+
+  console.log(issues);
   return (
     <div>
       <ul>
--- src/reducer/issues.js
+++ src/reducer/issues.js
@@ -1,9 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
-import fetchIssues from "../fetchIssues/fetchIssues";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { Octokit } from "@octokit/core";
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
 
 const initialState = {
   issues: null,
+  status: null,
 };
+
+export const asyncIssues = createAsyncThunk(
+  "issuesList/asyncFetch",
+  async () => {
+    const owner = "angular";
+    const repo = "angular-cli";
+    const response = await octokit.request(
+      "GET /repos/angular/angular-cli/issues",
+      {
+        owner: owner,
+        repo: repo,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
+    const data = response.data;
+    console.log(response.data);
+    return data;
+  }
+);
 
 export const issuesSlice = createSlice({
   name: "issues",
@@ -13,17 +39,18 @@ export const issuesSlice = createSlice({
       state.issues = action.payload;
     },
   },
+
+  extraReducers: (builder) => {
+    builder.addCase(asyncIssues.pending, (state, action) => {
+      state.status = "Loading";
+    });
+    builder.addCase(asyncIssues.fulfilled, (state, action) => {
+      state.issues = action.payload;
+    });
+    builder.addCase(asyncIssues.rejected, (state, action) => {
+      state.status = "Fail";
+    });
+  },
 });
 
 export const { getIssues } = issuesSlice.actions;
-
-export const fetchIssuesAsync = () => async (dispatch) => {
-  try {
-    const issues = await fetchIssues();
-    dispatch(getIssues(issues));
-  } catch (error) {
-    console.error("GitHub API 요청 오류:", error.message);
-  }
-};
-
-export default issuesSlice.reducer;
